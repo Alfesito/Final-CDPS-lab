84550734294aa3b8cb027456119c8c4e811e5f9f
Andrés Alfaro Fernández <78410971+Alfesito@users.noreply.github.com>
2022-12-27 10:54:39 +0100

    Create pc2-install.py

---
+++ pc2-install.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+
+# Actualizamos el sistema
+os.system('sudo apt-get update')
+os.system('sudo apt-get -y upgrade')
+
+# Verificar que Python y pip están instalados
+assert "python" in os.listdir("/usr/bin")
+assert "pip" in os.listdir("/usr/bin")
+# Instalamos Python y pip
+os.system('sudo apt-get -y install python3')
+os.system('sudo apt-get -y install python3-pip')
+
+# Clonamos el reppositorio de la practica
+os.system('git clone https://github.com/CDPS-ETSIT/practica_creativa2')
+
+# Instalar las dependencias especificadas en requirements.txt
+subprocess.check_call(["pip", "install", "-r", "./practica_creativa2/bookinfo/src/productpage/requirements.txt"])
+
+# Modificar el código de la aplicación para que en el título aparezca el nombre del grupo. que hay que cambiar?
+"""
+group_number = os.environ["GRUPO 40"]
+with open("./practica_creativa2/bookinfo/src/productpage/productpage_monolith.py", "r") as f:
+    code = f.read()
+code = code.replace("<GROUP_NUMBER>", group_number)
+with open("./practica_creativa2/bookinfo/src/productpage/productpage_monolith.py", "w") as f:
+    f.write(code)
+"""
+
+# Ejecutar la aplicación especificando el puerto deseado
+port = 8080  # Especificar el puerto deseado aquí
+subprocess.check_call(["python", "./practica_creativa2/bookinfo/src/productpage/productpage_monolith.py", str(port)])
