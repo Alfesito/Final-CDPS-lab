f9666c7ecd41391cfae630b9c5d5ed898cc3026a
Andrés Alfaro Fernández <78410971+Alfesito@users.noreply.github.com>
2022-12-27 14:03:05 +0100

    Update pc2-install.py

--- pc2-install.py
+++ pc2-install.py
@@ -13,8 +13,20 @@ os.system('sudo apt-get -y install python3.9-pip')
 os.system('sudo apt install git')
 os.system('git clone https://github.com/CDPS-ETSIT/practica_creativa2')
 
+# Modificamos el archivo requirements.txt con las versiones actualizadas
+
 # Instalar las dependencias especificadas en requirements.txt
 subprocess.check_call(["pip", "install", "-r", "./practica_creativa2/bookinfo/src/productpage/requirements.txt"])
+# Instalamos las librerias que dan problemas
+os.system('pip install urllib3')
+os.system('pip install visitor')
+os.system('pip install wrapt')
+os.system('pip install flask_bootstrap')
+os.system('pip install Flask Werkzeug -U')
+os.system('pip install jaeger-client')
+os.system('pip install opentracing-instrumentation')
+os.system('pip install json2html')
+os.system('pip install Flash')
 
 # Modificar el código de la aplicación para que en el título aparezca el nombre del grupo. que hay que cambiar?
 """
