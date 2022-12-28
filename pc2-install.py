5c2e655a3c6ca720f7502de19d00ffb3aa72792c
Andrés Alfaro Fernández <78410971+Alfesito@users.noreply.github.com>
2022-12-28 11:29:01 +0100

    Update pc2-install.py

--- pc2-install.py
+++ pc2-install.py
@@ -8,30 +8,28 @@ import os
 import subprocess
 
 # Actualizamos el sistema
-os.system('sudo apt-get update')
-os.system('sudo apt-get -y upgrade')
+#os.system('sudo apt-get update')
+#os.system('sudo apt-get -y upgrade')
 
 # Instalamos Python y pip
 os.system('sudo apt-get -y install python3.8')
 os.system('sudo apt-get -y install python3-pip')
 
 # Instalamos git y clonamos el repositorio de la practica
-os.system('sudo apt -y install git')
+os.system('sudo apt-get -y install git')
 os.system('git clone https://github.com/CDPS-ETSIT/practica_creativa2')
+
+# Modificamos el archivo requirements.txt debido a problemas entre versiones
+fin = open ("./practica_creativa2/bookinfo/src/productpage/requirements.txt","w+")
+for line in fin:
+    if line == "chardet==3.0.4":
+        fin.write("chardet\n")
+    if line == "urllib3==1.26.5":
+        fin.write("urllib3\n")
+fin.close()
 
 # Instalar las dependencias especificadas en requirements.txt
 subprocess.check_call(["pip", "install", "-r", "./practica_creativa2/bookinfo/src/productpage/requirements.txt"])
-# Instalamos las librerias que dan problemas
-os.system('pip install urllib3')
-os.system('pip install chardet')
-#os.system('pip install visitor')
-#os.system('pip install wrapt')
-#os.system('pip install flask_bootstrap')
-#os.system('pip install Flask Werkzeug -U')
-#os.system('pip install jaeger-client')
-#os.system('pip install opentracing-instrumentation')
-#os.system('pip install json2html')
-#os.system('pip install Flash')
 
 # Modificar el código de la aplicación para que en el título aparezca el nombre del grupo. que hay que cambiar?
 os.environ['GROUP_NUMBER'] = "Grupo 40"
@@ -41,7 +39,6 @@ with open("./practica_creativa2/bookinfo/src/productpage/templates/index.html", 
 code = code.replace("Simple Bookstore App", group_number)
 with open("./practica_creativa2/bookinfo/src/productpage/templates/index.html", "w") as f:
     f.write(code)
-
 
 # Ejecutar la aplicación especificando el puerto deseado
 port = 8080  # Especificar el puerto deseado aquí
