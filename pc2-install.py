b2e58c6e6801a6e0782b01b8f80b5e3e836e4470
Andrés Alfaro Fernández <78410971+Alfesito@users.noreply.github.com>
2022-12-28 11:20:28 +0100

    Update pc2-install.py

--- pc2-install.py
+++ pc2-install.py
@@ -19,9 +19,11 @@ os.system('sudo apt-get -y install python3-pip')
 os.system('sudo apt -y install git')
 os.system('git clone https://github.com/CDPS-ETSIT/practica_creativa2')
 
-
+# Instalar las dependencias especificadas en requirements.txt
+subprocess.check_call(["pip", "install", "-r", "./practica_creativa2/bookinfo/src/productpage/requirements.txt"])
 # Instalamos las librerias que dan problemas
-#os.system('pip install urllib3')
+os.system('pip install urllib3')
+os.system('pip install chardet')
 #os.system('pip install visitor')
 #os.system('pip install wrapt')
 #os.system('pip install flask_bootstrap')
@@ -30,9 +32,6 @@ os.system('git clone https://github.com/CDPS-ETSIT/practica_creativa2')
 #os.system('pip install opentracing-instrumentation')
 #os.system('pip install json2html')
 #os.system('pip install Flash')
-
-# Instalar las dependencias especificadas en requirements.txt
-subprocess.check_call(["pip", "install", "-r", "./practica_creativa2/bookinfo/src/productpage/requirements.txt"])
 
 # Modificar el código de la aplicación para que en el título aparezca el nombre del grupo. que hay que cambiar?
 os.environ['GROUP_NUMBER'] = "Grupo 40"
