a5ffbff823c68cb2e1f63f548f2bcc172cd42be1
Andrés Alfaro Fernández <78410971+Alfesito@users.noreply.github.com>
2022-12-28 12:51:36 +0100

    Update pc2-install.py

--- pc2-install.py
+++ pc2-install.py
@@ -12,7 +12,7 @@ os.system('sudo apt-get update')
 os.system('sudo apt-get -y upgrade')
 
 # Instalamos Python y pip
-#os.system('sudo apt-get -y install python3.8') #las versiones 3.8 y 3.9 son estables
+os.system('sudo apt-get -y install python3.9') #las versiones 3.8 y 3.9 son estables
 os.system('sudo apt-get -y install python3-pip')
 
 # Instalamos git y clonamos el repositorio de la practica
@@ -23,14 +23,9 @@ os.system('git clone https://github.com/CDPS-ETSIT/practica_creativa2')
 subprocess.check_call(["pip", "install", "-r", "./practica_creativa2/bookinfo/src/productpage/requirements.txt"])
 # Instalamos las librerias que dan problemas
 os.system('pip install urllib3')#
-os.system('pip install visitor')
-os.system('pip install wrapt')
 os.system('pip install flask_bootstrap')#
-os.system('pip install Flask Werkzeug -U')
 os.system('pip install jaeger-client')#
 os.system('pip install opentracing-instrumentation')#
-os.system('pip install json2html')
-os.system('pip install Flash')
 
 # Modificar el código de la aplicación para que en el título aparezca el nombre del grupo. que hay que cambiar?
 os.environ['GROUP_NUMBER'] = "Grupo 40"
