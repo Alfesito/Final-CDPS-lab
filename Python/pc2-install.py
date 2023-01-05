e540c5de2b3ead49bf46b662105bd4638430acda
Andrés Alfaro Fernández <andresalfe@gmail.com>
2023-01-05 09:32:41 +0100

    kube init

--- Python/pc2-install.py
+++ Python/pc2-install.py
@@ -9,6 +9,7 @@ import subprocess
 
 # Puerto del servicio
 port = 80
+# NOTA: la usar Google Cloud es importante poner el puerto 80, ya que de otra forma no se podrá mostrar la app
 
 # Actualizamos el sistema
 os.system('sudo apt-get -y update')
@@ -34,10 +35,10 @@ os.system('pip install flask_bootstrap')
 os.system('pip install jaeger-client')
 os.system('pip install opentracing-instrumentation')
 
-# Modificar el código de la aplicación para que en el título aparezca el nombre del grupo.
-#os.environ['GROUP_NUMBER'] = 40
-#group_number = os.environ['GROUP_NUMBER']
-group_number = 40
+# Declaramos la variable de entorno <GROUP_NUMBER>
+os.environ.setdefault('GROUP_NUMBER', '40')
+group_number = os.getenv('GROUP_NUMBER')
+# Modificamos el titulo de la app para que aparezca el valor de la variable de entorno.
 # Para index.html
 with open("./practica_creativa2/bookinfo/src/productpage/templates/index.html", "r") as f:
     code = f.read()
