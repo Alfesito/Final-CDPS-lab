129393b9c5d9355f2cbf25c8c270c7e947be3074
Andrés Alfaro Fernández <andresalfe@gmail.com>
2023-01-30 10:03:59 +0100

    sh version

--- Kubernetes/kube_script.sh
+++ Kubernetes/kube_script.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# Pasa como parametro la version de reviews que se quiere
+version = $1
 # Instalamos las imagenes de docker hub
 docker pull alfesito/productpage
 docker pull alfesito/details
@@ -11,4 +14,7 @@ kubectl apply -f productpage.yaml
 kubectl apply -f details.yaml
 kubectl apply -f ratings.yaml
 kubectl apply -f reviews-svc.yaml
-kubectl apply -f reviews-v3-deployment.yaml
+kubectl delete reviews-v1-deployment.yaml
+kubectl delete reviews-v2-deployment.yaml
+kubectl delete reviews-v3-deployment.yaml
+kubectl apply -f reviews-$version-deployment.yaml
